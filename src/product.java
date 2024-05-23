4fe8c3a003cdcf650d9e3e16de5203a274cd2058
shelleyng <shelleyn208@gmail.com>
2024-05-23 13:50:01 -0400

    first

--- src/product.java
+++ src/product.java
@@ -5,4 +5,32 @@
  **/
 public class product
 {
+    int productId;
+    String productName;
+    String category;
+    double price;
+
+    public product(int productId, String productName, String category, double price)
+    {
+        this.productId = productId;
+        this.productName = productName;
+        this.category = category;
+        this.price = price;
+    }
+
+    public int getProductId() {return productId;}
+
+    public void setProductId(int productId) {this.productId = productId;}
+
+    public String getProductName() {return productName;}
+
+    public void setProductName(String productName) {this.productName = productName;}
+
+    public String getCategory() {return category;}
+
+    public void setCategory(String category) {this.category = category;}
+
+    public double getPrice() {return price;}
+
+    public void setPrice(double price) {this.price = price;}
 }
--- src/productmanager.java
+++ src/productmanager.java
@@ -1,3 +1,7 @@
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
 /**
  * @author : shelley
  * @Description :
@@ -5,4 +9,31 @@
  **/
 public class productmanager
 {
+    public static ArrayList<product> products = new ArrayList<product>();
+
+public static void addproduct (int productId, String productName, String category, double price)
+{
+    product product = new product(productId, productName, category, price);
+    products.add(product);
+}
+ public static void removeproduct (int productId)
+ {
+     for (int i = 0; i < products.size(); i++)
+     {
+         if (products.get(i).getProductId() == productId)
+         {
+             products.remove(i);
+         }
+     }
+ }
+ public static void saveProductsToTextFile(String fileName)
+ {
+     try (PrintWriter writer = new PrintWriter(new FileWriter(fileName)))
+     {
+         for (product p : products)
+         {
+             writer.println()
+         }
+     }
+ }
 }
